4e4eefab908ffa2867198129577d21d47bd1219f
William Henney <will@henney.org>
2014-04-04 00:12:33 -0600

    Lots of work on the O II line diagnostics

---
+++ manu-extract-oii-4650.py
@@ -0,0 +1,31 @@
+from astropy.table import Table
+import argh
+import json
+
+linesets = {
+    "diag-blue": ["4363", "4639", "4649", "4651", "4662"],
+    "diag-green": ["4639", "4649", "4651", "4662",
+                   "4959", "5007", "5518", "5538"]
+}
+
+def main(dbname="manu_spectral_fit_db.json"):
+    with open(dbname) as f:
+        db = json.load(f)
+    tabs = {}
+    for name, lines in linesets.items():
+        colnames = ["x", "y"] + ["F"+line for line in lines]
+        dtypes = [float]*len(colnames)
+        tabs[name] = Table(names=colnames, dtypes=dtypes)
+
+    for posdata in db.values():
+        for name, lines in linesets.items():
+            try:
+                rowdata = [posdata[line]["Flux"] for line in lines]
+                tabs[name].add_row([posdata['x'], posdata['y']] + rowdata)
+            except KeyError:
+                pass
+    for name, lines in linesets.items():
+        tabs[name].write("manu-{}-fluxes.tab".format(name), format='ascii.tab')
+
+if __name__ == "__main__":
+    argh.dispatch_command(main)
