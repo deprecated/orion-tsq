399a923cb6ef4fa80d78936c2a56e0515271d393
William Henney <will@henney.org>
2014-07-06 00:56:36 -0500

    Make things much better with the calibrations
    + Use accurate wavelengths
    + Include velocity shifts
    + Allow for calibration adjustments to T_i^j (e.g, 0.9 for 6583 in
      F658N)
    + Stick a whole lot of new stuff in the appendix of draft MS

--- wfc3_utils.py
+++ wfc3_utils.py
@@ -9,6 +9,34 @@ GAIN = 1.5
 ARCSEC_RADIAN = 180.0*3600.0/np.pi 
 
 AIR_REFRACTIVE_INDEX = 1.000277 # @STP according to Wikipedia
+LIGHTSPEED = 2.99792458e5       # c in km/s
+
+
+# Adjustments to nominal values of the line transmission in a given band
+line_transmission_adjustments = {
+    (5755, "FQ575N"): 1.0,      # example syntax
+    # Additional items may be added by the caller
+}
+
+# Accurate rest wavelengths
+air_rest_wavelength = {
+    4340: 4340.47,
+    4363: 4363.21,
+    4861: 4861.63,
+    5007: 5006.84,
+    5755: 5755.08,
+    6563: 6562.79,
+    6583: 6583.45,
+    6716: 6716.44,
+    6731: 6730.816
+}
+
+# Radial velocity of source wrt HST
+# Default value is suitable for Orion Nebula with no HST-sun correction
+# But note that earth-sun motion may be +/- 30 km/s
+# And also HST-earth motion may be +/- 8 km/s
+topocentric_velocity = 20.0     
+
 
 def get_filter(fname, UVIS=1, return_wavelength=False):
     datafile = "{:s}.UVIS{:01d}.tab".format(fname.lower(), UVIS)
@@ -19,6 +47,7 @@ def get_filter(fname, UVIS=1, return_wavelength=False):
         return data["wavelength"], data["throughput"]
     else:
         return data["throughput"]
+
 
 def get_interpolated_filter(fname, wavs, UVIS=1):
     """
@@ -38,16 +67,22 @@ def Wj(wavs, T):
     return np.trapz(T, wavs)/Tm(T)
 
 
-def Ti(wav0, wavs, T):
+def vacuum_wavelength(wav0):
+    "Convert from wav identifier to an accurate (0.01 Ang) vacuum wavelength"
+    return air_rest_wavelength[wav0] \
+        *AIR_REFRACTIVE_INDEX*(1.0 + topocentric_velocity/LIGHTSPEED)
+
+
+def Ti(wav0, wavs, fname, T):
     "Filter transmission at wavelength of line i (wav0)"
-    return np.interp(wav0*AIR_REFRACTIVE_INDEX, wavs, T)
+    correction = line_transmission_adjustments.get((wav0, fname), 1.0)
+    return correction*np.interp(vacuum_wavelength(wav0), wavs, T)
 
 
-def Wtwid(wav0, wavs, T, kji=1.0):
+def Wtwid(wav0, wavs, fname, T, kji=1.0):
     """Find W-twiddle for a given line of wavelength wav0
     with respect to a filter transmission curve T(wavs)"""
-    # We are still missing the k_{j,i} term 
-    return kji*Tm(T)*Wj(wavs, T)/Ti(wav0, wavs, T)
+    return kji*Tm(T)*Wj(wavs, T)/Ti(wav0, wavs, fname, T)
 
 
 def ratio_coefficients(wav1=5755, wav2=6584, I="FQ575N", II="F658N", III="F547M"):
@@ -55,12 +90,12 @@ def ratio_coefficients(wav1=5755, wav2=6584, I="FQ575N", II="F658N", III="F547M"
     T_II = get_filter(II)
     T_III = get_filter(III)
 
-    T_1_III = Ti(wav1, wavs, T_III)
-    T_2_III = Ti(wav2, wavs, T_III)
-    T_1_I = Ti(wav1, wavs, T_I)
-    T_2_II = Ti(wav2, wavs, T_II)
-    T_1_II = Ti(wav1, wavs, T_II)
-    T_2_I = Ti(wav2, wavs, T_I)
+    T_1_III = Ti(wav1, wavs, III, T_III)
+    T_2_III = Ti(wav2, wavs, III, T_III)
+    T_1_I = Ti(wav1, wavs, I, T_I)
+    T_2_II = Ti(wav2, wavs, II, T_II)
+    T_1_II = Ti(wav1, wavs, II, T_II)
+    T_2_I = Ti(wav2, wavs, I, T_I)
 
     Tm_I = Tm(T_I)
     Tm_II = Tm(T_II)
@@ -71,6 +106,7 @@ def ratio_coefficients(wav1=5755, wav2=6584, I="FQ575N", II="F658N", III="F547M"
     W_III = Wj(wavs, T_III)
 
     return {
+        "T2/T1": T_2_II/T_1_I,
         "alpha_1": T_1_III/T_1_I,
         "alpha_2": T_2_III/T_2_II,
         "beta_1": Tm_I*W_I/(Tm_III*W_III),
@@ -90,8 +126,8 @@ If `naive` is True, then ignore the continuum and line contamination terms
     wav2 = filterset["wav2"]
     wavs, T_I = get_filter(filterset['I'], return_wavelength=True)
     T_II = get_filter(filterset['II'])
-    T_1_I = Ti(wav1, wavs, T_I)
-    T_2_II = Ti(wav2, wavs, T_II)
+    T_1_I = Ti(wav1, wavs, filterset['I'], T_I)
+    T_2_II = Ti(wav2, wavs, filterset['II'], T_II)
 
     if naive:
         ratio = R_I/R_II
